362af2144252a172dd92896c22deec908fdd18ae
mohammadsleman167819 <mo.sleman.000@gmail.com>
2024-10-20 18:05:13 +0300

    add pagination and navbar to home page

--- mainapp/views/home.py
+++ mainapp/views/home.py
@@ -1,9 +1,10 @@
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
-from ..forms.home import CustomLoginForm
 from django.shortcuts import redirect, render
 from django.contrib.auth.mixins import UserPassesTestMixin
 from ..models import Post
+from ..forms.home import CustomLoginForm
 
 class SignUpView(UserPassesTestMixin, TemplateView):
     template_name = 'registration/signup.html'
@@ -26,9 +27,29 @@ class LoginView(UserPassesTestMixin, LoginView):
         return redirect('index')
 
 
+
 def index(request):
-    posts = Post.objects.all()
-    return render(request, 'mainapp/index.html', {'posts': posts})
+    nav = request.GET.get('nav', 'posts')
+    
+    if nav == 'verified':
+        post_list = Post.objects.filter(status="Verfied")
+    elif nav == 'disproven':
+        post_list = Post.objects.filter(status="Disproven")
+    else:
+        post_list = Post.objects.all()
+    
+    paginator = Paginator(post_list, 10)  # Show 10 posts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        context = {'page_obj': page_obj}
+        return render(request, 'mainapp/post/_posts_section.html', context)
+    
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def custom_403_view(request, exception=None):
     return render(request, '403.html', status=403)
