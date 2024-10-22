8c15392010d3174cca77303da2e5dc2b422ccf01
mohammadsleman167819 <mo.sleman.000@gmail.com>
2024-10-23 02:35:51 +0300

    add gmail smtp

--- posts/settings.py
+++ posts/settings.py
@@ -149,4 +149,11 @@ LOGIN_REDIRECT_URL = "index"
 
 LOGOUT_REDIRECT_URL = "index"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "svu123hrplus@gmail.com"
+EMAIL_HOST_PASSWORD = 'upegbhkcwdeacybt '
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
