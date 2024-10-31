335b5555bc58073e399151c0e923ae67772b6812
mohammadsleman167819 <mo.sleman.000@gmail.com>
2024-10-31 17:34:36 +0300

    add test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import CustomUser
 
-# Create your tests here.
+class AuthenticationTestCase(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email='test@test.com', password='testpassword')
+
+    def test_valid_login(self):
+        login = self.client.login(email='test@test.com', password='testpassword')
+        self.assertTrue(login)
+
+    def test_invalid_login(self):
+        login = self.client.login(email='test@test.com', password='wrongpassword')
+        self.assertFalse(login)
